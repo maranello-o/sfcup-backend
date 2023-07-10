f319e3f14c6ae27fcc612b3ee15dc86bfb059b40
Maranello <sh.maranello@gmail.com>
2023-07-10 11:16:14 +0800

    fix: 修正个人使用记录接口

--- handler/usage_handler.go
+++ handler/usage_handler.go
@@ -8,14 +8,24 @@ import (
 	"time"
 )
 
+type GetSelfUsageDTO struct {
+	Model string
+	Time  string
+}
+
 func GetSelfUsage(c *gin.Context) {
+	var usages []GetSelfUsageDTO
 	id := c.MustGet("id").(int64)
 	result, err := dal.Usage.Where(dal.Usage.UserID.Eq(id)).Find()
 	if err != nil {
 		response.Send(c, http.StatusInternalServerError, nil, "服务器错误")
 		return
 	}
-	response.Send(c, http.StatusOK, result, "")
+	for _, v := range result {
+		usage := GetSelfUsageDTO{Model: v.Model, Time: time.Unix(v.CreateAt, 0).Format("2006-01-02 15:04:05")}
+		usages = append(usages, usage)
+	}
+	response.Send(c, http.StatusOK, usages, "")
 }
 
 func GetTotalUsageStatistic(c *gin.Context) {
